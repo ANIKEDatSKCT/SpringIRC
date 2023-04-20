4b57e309597ea3058260cc9fc7ec5f9a10cf9818
Aniked E.P <anikedanil@gmail.com>
2023-04-21 00:04:29 +0530

    day2

--- Day2/src/main/java/com/example/day1/Day2Application.java
+++ Day2/src/main/java/com/example/day1/Day2Application.java
@@ -1,13 +1,37 @@
 package com.example.day1;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Day2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Day2Application.class, args);
+		
+	}
+	String name="Aniked";
+	@Value("${valValue:Aniked.E.P}")
+	String val;
+	
+	@GetMapping("/hello")
+	public String welcome()
+	{
+		return "Hello world";
+	}
+	@GetMapping("/aniked")
+	public String display()
+	{
+		return "Welcome "+name;
+	}
+	@GetMapping("/value")
+	public String value()
+	{
+		return "welcome "+val;
 	}
 
 }
